eec59c778e1c8dfc0e5409f5f82969ffbae8d883
ryan_shadow <ryan_shadow@example.com>
2021-06-15 04:05:27 +0900

    README修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_13_063941) do
-
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.string "url"
-    t.string "price"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_items_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2021_06_13_042343) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "line_id"
@@ -28,5 +18,4 @@ ActiveRecord::Schema.define(version: 2021_06_13_063941) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "items", "users"
 end
