b333ae23416eba112d91d8afa10ce1eceaa51e69
ryan_shadow <ryan_shadow@example.com>
2021-06-15 04:11:17 +0900

    不要ファイル削除

--- app/models/item.rb
+++
@@ -1,8 +0,0 @@
-class Item < ApplicationRecord
-  validates :name, presence: true
-  validates :url, presence: true
-  validates :price, presence: true
-  validates :user_id, presence: true
-
-  belongs_to :user
-end
